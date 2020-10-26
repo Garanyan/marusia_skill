4e09c7c93b65a9ee9f2f9b9835f6a404fc0f60f6
Garanyan Ovanes <o.garanyan@itgrp.ru>
2020-10-26 11:00:24 +0300

    fix flask app

--- app.py
+++ app.py
@@ -7,7 +7,16 @@ app = Flask(__name__)
 
 @app.route("/marusya", methods=["POST", "GET"])
 def marusya():
-	return "Marusya"
+    return "Marusya"
 
 @app.route("/visa", methods=['POST'])
-	return "Visa post"
+def vise():
+    return "Visa post"
+
+@app.route("/")
+def home():
+    return "Welcome page!"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
