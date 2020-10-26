2b7f5896fcc4ea7e9297b49371c9411ed7bafb22
Garanyan Ovanes <o.garanyan@itgrp.ru>
2020-10-26 11:26:20 +0300

    add images

--- app.py
+++ app.py
@@ -10,8 +10,56 @@ def marusya():
     return "Marusya"
 
 @app.route("/visa", methods=['POST'])
-def vise():
-    return "Visa post"
+def visa():
+    card = {}
+    buttons = []
+    text = "эх"
+    if request.json['session']['new']:
+        text = "Привет это скилл Виза"
+    elif request.json['request']['command'] == 'on_interrupt':
+        text = "Пока, возвращайся, когда отправишься в другую страну."
+    elif request.json['request']['command'].lower() == 'привет' or request.json['request']['original_utterance'].lower() == 'привет':
+        text = "Здравствуйте."
+    elif request.json['request']['command'].lower() == 'картинка':
+        card = {
+        "type": 'BigImage',
+        "image_id": 457239017,
+        "title": "Красота",
+        "description": ""
+
+        }
+    elif request.json['request']['command'].lower() == 'карусель':
+        card = {
+        "type": 'ItemsList',
+        "items": [457239018, 457239019, 457239017],
+        "title": "",
+        "description": ""
+        }
+    elif request.json['request']['command'].lower() == 'кнопки':
+        buttons = [
+              {
+                "title": "1"
+              },
+              {
+                "title": "2"
+              },
+              {
+                "title": "3"
+              }
+        ]
+
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        'response':{
+            "end_session":False,
+            "text":text,
+            "card":card,
+            "buttons": buttons
+        }
+    }
+    return json.dumps(response, ensure_ascii=False, intent=2)
 
 @app.route("/")
 def home():
